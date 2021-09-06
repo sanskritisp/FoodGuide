b1d4849379f5c8d8007595dbe24e8c63e537acca
SanskritiPandey <khushiksp17@gmail.com>
2021-09-06 16:56:18 -0700

    feat:list updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
+
+ const SouthIndian=[
+  
+   {name: "DOSA",
+    descrp: "A dosa is a thin pancake or crepe originating from South India, made from a fermented batter predominantly consisting of lentils and rice.",
+    rate: "8/10"},
+  
+  {
+    name: "PONGAL",
+    descrp: "Pongal is a sweet rice dish that is usually eaten during special or ceremonial occasions.It is usually cooked in a clay pot over an open fire.",
+    rate: "7.5/10"
+  },
+  {
+    name: "UPMA",
+    descrp: "Upma is a wholesome Indian dish made with dry semolina or rice flour, cooked into a thick porridge. ",
+    rate: "7/10"
+  }
+];
+ const NorthIndian=[
+  {
+    name: "RAJMA CHAWAL",
+    descrp: "Rajma is one of the most popular vegetarian dishes of North India. It consists of red kidney beans which are stewed in a rich gravy combined with numerous spices.",
+    rate: "9/10"
+  },
+  {
+    name: "TANDORI CHICKEN",
+    descrp: "Tandoori chicken or tandoori murgh is one of the most popular dishes in Indian cuisine, its name derived from the Persian word tannur, meaning fire.",
+    rate: "8/10"
+  },
+  {
+    name: "SHAHI PANEER",
+    descrp: "Originating from India's Moghul cuisine, shahi paneer is a cheese curry that is prepared with paneer cheese, onions, almond paste, and a rich, spicy tomato-cream sauce.",
+    rate: "8.5/10"
+  }
+];
+ const Chinese=[
+  {
+    name: "CHOW-MEIN",
+    descrp: "It is one of the best Chinese dishes, usually made of noodles, onion, sliced meat, green pepper and green onion. ",
+    rate: "8/10"
+  },
+  {
+    name: "SPRING ROLLS",
+    descrp: "Spring rolls are a Cantonese dim sum of cylindrical shape. The filling of spring rolls could be vegetables or meat, and the taste could be either sweet or savory.",
+    rate: "8.5/10"
+  },
+  {
+    name: "MANCHURIAN",
+    descrp : "If you love the unique flavour and taste of this semi-Chinese flavour with subtle Indian flavours, you can make healthy Manchurian at home. Healthy Manchurian Calories per serving are 205 calories.",
+    rate: "9/10"
+  }
+];
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
